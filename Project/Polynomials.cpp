aeb55fc918eddf52763d17fa43806f1ad6289e21
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2022-05-20 10:13:50 +0530

    First commit, added polynomials.cpp

---
+++ Project/Polynomials.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class Polynomial{
+    int *degCoff;
+    int capacity = 100;
+    public:
+        Polynomial(){
+            degCoff = new int[capacity];
+            for(int i=0; i<capacity; i++){
+                degCoff[i] = 0;
+            }
+        }
+
+        void set(int deg, int coff){
+
+            int *newDegCoff;            
+
+            if(deg > capacity){
+                int size = capacity;
+                while(deg >= size){
+                    size *= 2;
+                }
+
+                newDegCoff = new int[size];
+
+                for(int i=0; i<size; i++){
+                    if(i < capacity){
+                        newDegCoff[i] = degCoff[i];
+                    }
+                    else{
+                        newDegCoff[i] = 0;
+                    }
+                }
+                capacity = size;
+                delete [] degCoff;
+                degCoff = newDegCoff;
+            }
+            degCoff[deg] = coff;
+        }
+};
+
+int main(){
+
+    Polynomial p1;
+
+    return 0;
+}
